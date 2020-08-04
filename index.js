91684fa73a3f408a59e95ff1048c35392c32eace
Michael Canon <michaeljamescanon@gmail.com>
2020-08-04 17:21:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // write your code below!
 var name = "Joe"
 var height = 74
-var extra = "and is that dude"
-var message = `${name} is ${height} inches tall ${extra}`
+var message = `${name} is ${height} inches tall`
 console.log(message)
 module.exports = {name, height, message}
